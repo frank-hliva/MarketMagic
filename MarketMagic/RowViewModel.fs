35d32c497a778428ad1a417a4de944ecca6d0f06
Frnak Hliva <fhliva@outlook.com>
2025-08-01 01:08:59 +0200

    fix: IsNew property added to RowViewModel

--- MarketMagic/RowViewModel.fs
+++ MarketMagic/RowViewModel.fs
@@ -9,13 +9,37 @@ open System.Collections.ObjectModel
 open System.ComponentModel
 open System.Runtime.CompilerServices
 
-type RowViewModel(cells: string[]) =
+type RowViewModel(props : {| cells: string[]; isNew : bool |}) =
     inherit BasicViewModel()
 
-    let mutable cellValues = cells
+    let mutable cellValues = props.cells
+    let mutable isNew = props.isNew
 
     member self.Item
         with get(i) = cellValues[i]
         and set(i) value =
             cellValues[i] <- value
             self.OnPropertyChanged($"Item[{i}]")
+            self.IsNew <- false
+
+    member self.IsNew
+        with get() = isNew
+        and private set(value) =
+            if isNew <> value then
+                isNew <- value
+                self.OnPropertyChanged(nameof self.IsNew)
+
+    static member New(columnLength : int) =
+        RowViewModel({|
+            cells = Array.create columnLength ""
+            isNew = true
+        |})
+
+    static member New(columns : string[]) =
+        RowViewModel.New(columns.Length)
+
+    new(cells: string[]) =
+        RowViewModel({|
+            cells = cells
+            isNew = false
+        |})
